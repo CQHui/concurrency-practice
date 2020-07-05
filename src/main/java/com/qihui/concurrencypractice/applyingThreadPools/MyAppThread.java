94bd0278b500ed0f6dfd854c33055c9d340b69e7
chenqihui <zjhzcqh@163.com>
2020-07-05 22:29:23 +0800

    Applying Thread Pools

---
+++ src/main/java/com/qihui/concurrencypractice/applyingThreadPools/MyAppThread.java
@@ -0,0 +1,61 @@
+package com.qihui.concurrencypractice.applyingThreadPools;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Set a custom UncaughtExceptionHandler that write a message to logger.
+ */
+public class MyAppThread extends Thread {
+    public static final String DEFAULT_VALUE = "MyThread";
+    private static volatile boolean debugLifeCycle = false;
+    private static final AtomicInteger created = new AtomicInteger();
+    private static final AtomicInteger alive = new AtomicInteger();
+    private static final Logger log = Logger.getAnonymousLogger();
+
+    public MyAppThread(Runnable runnable) {
+        new MyAppThread(runnable, DEFAULT_VALUE);
+    }
+
+    public MyAppThread(Runnable runnable, String name) {
+        super(runnable, name + "-" + created.incrementAndGet());
+        setUncaughtExceptionHandler(
+                (t, e) -> log.log(Level.SEVERE, "Uncaught in thread " + t.getName(), e)
+        );
+    }
+    @Override
+    public void run() {
+        boolean debug = debugLifeCycle;
+        if (debug) {
+            log.log(Level.FINE, "Created " + getName());
+        }
+        try {
+            alive.incrementAndGet();
+            super.run();
+        } finally {
+            alive.decrementAndGet();
+            if (debug) {
+                log.log(Level.FINE, "Created " + getName());
+            }
+        }
+    }
+
+    public static boolean isDebugLifeCycle() {
+        return debugLifeCycle;
+    }
+
+    public static int getCreated() {
+        return created.get();
+    }
+
+    public static int getAlive() {
+        return alive.get();
+    }
+
+    public static void setDebugLifeCycle(boolean debugLifeCycle) {
+        MyAppThread.debugLifeCycle = debugLifeCycle;
+    }
+}
---
+++ src/main/java/com/qihui/concurrencypractice/applyingThreadPools/ThreadDeadlock.java
@@ -0,0 +1,39 @@
+package com.qihui.concurrencypractice.applyingThreadPools;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * Deadlock in a single-thread Executor. Don't do this
+ */
+public class ThreadDeadlock {
+    ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+    public RenderPageTask getRenderPageTask() {
+        return new RenderPageTask();
+    }
+
+    public class RenderPageTask implements Callable<String> {
+
+        @Override
+        public String call() throws Exception {
+            Future<String> header, footer;
+            header = executorService.submit(() -> {
+                String headerContent = "header";
+                System.out.println(headerContent);
+                Thread.sleep(1000L);
+                return headerContent;
+            });
+            footer = executorService.submit(() -> {
+                String footerContent = "footer";
+                System.out.println(footerContent);
+                Thread.sleep(1000L);
+                return footerContent;
+            });
+            return header.get() + " and " +  footer.get();
+        }
+    }
+
+}
